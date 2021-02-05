9b4614d617262c7fd3ff84f4308c1b1120da6ca8
BrienB <barrdom@hotmail.com>
2021-02-05 00:18:45 -0500

    initial commit

---
+++ seed.sql
@@ -0,0 +1,32 @@
+INSERT INTO employee (first_name, last_name)
+VALUES ("John", "Doe"),
+("Amy", "Anderson"),
+("Peter", "Graves"),
+("Alan", "Hammond"),
+("Bob", "Baker"),
+("Henry", "Cole"),
+("Jennifer", "Jacobs"),
+("Finley", "Barr"),
+("Frankie", "Barr");
+
+INSERT INTO role (title, salary)
+VALUES ("Salesperson", 60000.00),
+("Sales Lead", 80000.00),
+("Lead Engineer", 130000.00),
+("Software Engineer", 80000.00),
+("Accountant", 100000.00),
+("Legal Team Lead", 150000.00),
+("Lawyer", 120000.00),
+("Software Engineer", 85000.00),
+("Lawyer", 90000.00);
+
+INSERT INTO department (name)
+VALUES ("Sales"),
+("Sales"),
+("Engineering"),
+("Engineering"),
+("Finance"),
+("Legal"),
+("Legal"),
+("Engineering"),
+("Legal");
