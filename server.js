ab863f7deecf35284b21111bb748522e96a4ab55
BrienB <barrdom@hotmail.com>
2021-02-05 00:57:01 -0500

    initial commit with view all employees

---
+++ server.js
@@ -0,0 +1,87 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+const cTable = require('console.table');
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Darwin@Maddy6",
+  database: "employee_trackerDB"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  runTracker();
+});
+
+function runTracker() {
+  inquirer
+    .prompt({
+      name: "action",
+      type: "rawlist",
+      message: "What would you like to do?",
+      choices: [
+        "View All Employees",
+        "View All Employees By Department",
+        "View All Employees By Manager",
+        "Add Employee",
+        "Remove Employee",
+        "Update Employee Role",
+        "Update Employee Manager",
+        "Exit"
+      ]
+    })
+    .then(function(answer) {
+      switch (answer.action) {
+      case "View All Employees":
+        viewEmployees();
+        break;
+
+      case "View All Employees By Department":
+        viewByDepartment();
+        break;
+
+      case "View All Employees By Manager":
+        viewByManager();
+        break;
+
+      case "Add Employee":
+        addEmployee();
+        break;
+
+      case "Remove Employee":
+        removeEmployee();
+        break;
+
+      case "Update Employee Role":
+        updateRole();
+        break;
+
+      case "Update Employee Manager":
+        updateManager();
+        break;
+
+      case "Exit":
+        // f();
+        break;
+      }
+    });
+};
+
+function viewEmployees(){
+  var query = "SELECT employee.*, role.title, role.salary, department.name as department from employee\n";
+  query += "INNER JOIN role on employee.id = role.id\n";
+  query += "INNER JOIN department on employee.id = department.id";
+    connection.query(query, async function(err, res) {
+      await console.table(res);  
+      runTracker();
+    })
+    
+}
